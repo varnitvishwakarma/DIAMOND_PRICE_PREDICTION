aceaa66e889c952dd0129059b5bd5ea871e45d26
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-08-16 13:08:40 +0530

    Update application.py

--- application.py
+++ application.py
@@ -34,7 +34,7 @@ def predict_datapoint():
 
         results=round(pred[0],2)
 
-        return render_template('foam.html',final_result=results)
+        return render_template('foam.html',final_result=results*80)
         
     
 
